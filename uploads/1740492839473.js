38e76acb366842fdd466396f6c9d0c1ab8b06634
rjayaprakashnarayanarao <rjayaprakashnarayanarao@gmail.com>
2025-02-25 19:48:39 +0530

    updated few design-2

---
+++ uploads/1740492839473.js
@@ -0,0 +1,41 @@
+console.log("This site is under RJP's rule");
+
+document.getElementById('resetPasswordBtn').addEventListener('click', function () {
+    const email = document.getElementById('identifier').value;
+
+    // Validation
+    if (!email) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+    // Prepare the data to be sent
+    const data = { email };
+
+    // Make a POST request
+    fetch('/api/auth/forgetPassword', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log("result: ",result);
+                
+        if (result.type =="success") {
+            //set emailID in localstorage
+            sessionStorage.setItem('email', email);
+            //redirect to verificationPage.html
+            window.location.href = 'verificationPage.html';
+            // alert('Password reset link sent to your email.');
+        } else {
+            alert('Failed to reset password. Please try again.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again later.');
+    });
+});
